67b695e9cc6a9f3caa50d6fd693cce29870b1fc4
dChambers14 <jstanesa@mail.umw.edu>
2014-03-11 16:39:18 +0000

    Added estate adder

--- EFserver.py
+++ EFserver.py
@@ -7,7 +7,6 @@ app = Flask(__name__)
 def mainIndex():
     return render_template('index.html')
   
-
 @app.route('/houses', methods=['GET', 'POST'])
 def report3():
   
@@ -18,6 +17,25 @@ def report3():
     rows = cur.fetchall()
 
     return render_template('houses.html', houses=rows)
+  
+@app.route('/add', methods=['GET', 'POST'])
+def estateadd():
+
+    return render_template('add.html')
+  
+@app.route('/add2', methods=['GET', 'POST'])
+def estateadd2():
+    db = utils.db_connect()
+    cur = db.cursor()    
+    #if user typed in the form and submitted
+    if request.method == 'POST':
+      query = "INSERT INTO basicHouse VALUES ('" + request.form['address'] +"', '"+request.form['date']+"', '"+request.form['county']+"', '"+request.form['state']+"', "+request.form['price']+")"
+      print(query)
+      cur.execute(query)
+      #rows = cur.fetchall()
+      db.commit()
+    return render_template('index.html')
+
 @app.route('/locateForm', methods=['GET', 'POST'])
 def estatelocate():
 
@@ -38,15 +56,16 @@ def estatelocate2():
 @app.route('/locateReturn', methods=['GET', 'POST'])
 def report4():
 
-    
     db = utils.db_connect()
     cur = db.cursor(cursorclass=MySQLdb.cursors.DictCursor)
-
 
     cur.execute(query)
     rows = cur.fetchall()
 
     return render_template('locateReturn.html', selectedMenu='List')
+@app.route('/contact')
+def contact():
+  return render_template('contact.html')
 
 if __name__ == '__main__':
     app.debug=True
